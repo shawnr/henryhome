f6296b3d8912fd896b8f4456267135301d43252d
Shawn Rider <shawn@shawnrider.com>
2017-03-26 15:15:49 -0700

    Initial commit.
    Change-Id: If684aba1b783bced9adca4d76671420901a30126

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name henryhomeApp
+ * @description
+ * # henryhomeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('henryhomeApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
